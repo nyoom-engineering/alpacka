d3406196d90ea85428c4042acfc2517f0c63494b
zackartz <zackmyers@lavabit.com>
2023-01-31 23:42:44 -1000

    feat: add commit history

--- src/smith/mod.rs
+++ src/smith/mod.rs
@@ -74,6 +74,16 @@ pub trait Smith: FmtDebug + Send + Sync {
     /// This function will return an error if the package cannot be resolved.
     fn resolve(&self, package: &Package) -> ErrorStackResult<Self::Input, ResolveError>;
 
+    /// Get latest commits for a git repo,
+    ///
+    /// # Errors
+    /// This function will return an error if it cannot find the commits
+    fn get_latest_commits(
+        &self,
+        old_sha: Option<git2::Oid>,
+        path: &Path,
+    ) -> ErrorStackResult<Vec<String>, LoadError>;
+
     /// Loads a package.
     /// This downloads and installs the package to the given directory.
     ///
